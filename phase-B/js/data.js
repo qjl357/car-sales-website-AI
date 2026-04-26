1efac46775f204e0e0b7b81c19fb73e652b006d8
nrain <205319469@qq.com>
2026-04-26 17:49:25 +0800

    Add files via upload

---
+++ phase-B/js/data.js
@@ -0,0 +1,17 @@
+window.AppData = {
+  hotRows: [
+    {
+      label: "Brand / Model",
+      tags: ["BMW", "Audi", "Accord", "Camry", "Mercedes-Benz", "Volkswagen", "Tesla", "BYD"],
+    },
+    {
+      label: "Type",
+      tags: ["SUV", "EV", "Large Sedan", "MPV", "Sedan", "Hybrid"],
+    },
+    {
+      label: "Price",
+      tags: ["Under 50k", "50k-100k", "100k-150k", "150k-200k", "200k-300k", "300k-500k", "500k+"],
+    },
+  ],
+  rankingPlaceholderRows: 5,
+};
